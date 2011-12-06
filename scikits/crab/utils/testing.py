92bf15465cb2cb46ca89b56e6af6ad79c6971687
Marcel Caraciolo <caraciol@gmail.com>
2011-12-06 17:31:48 -0300

    Added Counter class and new testing utilities.

--- scikits/crab/utils/testing.py
+++ scikits/crab/utils/testing.py
@@ -2,6 +2,8 @@
 
 # Copyright (c) 2011 Marcel Caraciolo <marcel@muricoca.com>
 # License: Simplified BSD
+import numpy as np
+from distutils.version import LooseVersion
 
 
 def assert_in(obj, in_=None, out_=None):
@@ -13,3 +15,135 @@ def assert_in(obj, in_=None, out_=None):
     if out_ is not None:
         for name in out_:
             assert name not in obj
+
+
+def equal_contents(arr1, arr2):
+    """Checks if the set of unique elements of arr1 and arr2 are equivalent.
+    """
+    return frozenset(arr1) == frozenset(arr2)
+
+
+def isiterable(obj):
+    return hasattr(obj, '__iter__')
+
+
+def assert_almost_equal(a, b):
+    if isinstance(a, dict) or isinstance(b, dict):
+        return assert_dict_equal(a, b)
+
+    if isinstance(a, basestring):
+        assert a == b, (a, b)
+        return True
+
+    if isiterable(a):
+        np.testing.assert_(isiterable(b))
+        np.testing.assert_equal(len(a), len(b))
+        if np.array_equal(a, b):
+            return True
+        else:
+            for i in xrange(len(a)):
+                assert_almost_equal(a[i], b[i])
+        return True
+
+    err_msg = lambda a, b: 'expected %.5f but got %.5f' % (a, b)
+
+    if isnull(a):
+        np.testing.assert_(isnull(b))
+        return
+
+    if isinstance(a, (bool, float, int)):
+        # case for zero
+        if abs(a) < 1e-5:
+            np.testing.assert_almost_equal(
+                a, b, decimal=5, err_msg=err_msg(a, b), verbose=False)
+        else:
+            np.testing.assert_almost_equal(
+                1, a / b, decimal=5, err_msg=err_msg(a, b), verbose=False)
+    else:
+        assert(a == b)
+
+
+def is_sorted(seq):
+    return assert_almost_equal(seq, np.sort(np.array(seq)))
+
+
+def assert_dict_equal(a, b, compare_keys=True):
+    a_keys = frozenset(a.keys())
+    b_keys = frozenset(b.keys())
+
+    if compare_keys:
+        assert(a_keys == b_keys)
+
+    for k in a_keys:
+        assert_almost_equal(a[k], b[k])
+
+
+def assert_contains_all(iterable, dic):
+    for k in iterable:
+        assert(k in dic)
+
+
+# Dependency checks.  Copied this from Nipy/Nipype (Copyright of
+# respective developers, license: BSD-3)
+def package_check(pkg_name, version=None, app='crab', checker=LooseVersion,
+                  exc_failed_import=ImportError,
+                  exc_failed_check=RuntimeError):
+    """Check that the minimal version of the required package is installed.
+
+    Parameters
+    ----------
+    pkg_name : string
+        Name of the required package.
+    version : string, optional
+        Minimal version number for required package.
+    app : string, optional
+        Application that is performing the check.  For instance, the
+        name of the tutorial being executed that depends on specific
+        packages.
+    checker : object, optional
+        The class that will perform the version checking.  Default is
+        distutils.version.LooseVersion.
+    exc_failed_import : Exception, optional
+        Class of the exception to be thrown if import failed.
+    exc_failed_check : Exception, optional
+        Class of the exception to be thrown if version check failed.
+
+    Examples
+    --------
+    package_check('numpy', '1.3')
+    package_check('networkx', '1.0', 'tutorial1')
+
+    """
+
+    if app:
+        msg = '%s requires %s' % (app, pkg_name)
+    else:
+        msg = 'module requires %s' % pkg_name
+    if version:
+        msg += ' with version >= %s' % (version,)
+    try:
+        mod = __import__(pkg_name)
+    except ImportError:
+        raise exc_failed_import(msg)
+    if not version:
+        return
+    try:
+        have_version = mod.__version__
+    except AttributeError:
+        raise exc_failed_check('Cannot find version for %s' % pkg_name)
+    if checker(have_version) < checker(version):
+        raise exc_failed_check(msg)
+
+
+def skip_if_no_package(*args, **kwargs):
+    """Raise SkipTest if package_check fails
+
+    Parameters
+    ----------
+    *args Positional parameters passed to `package_check`
+    *kwargs Keyword parameters passed to `package_check`
+    """
+    from nose import SkipTest
+    package_check(exc_failed_import=SkipTest,
+                  exc_failed_check=SkipTest,
+                  *args, **kwargs)
